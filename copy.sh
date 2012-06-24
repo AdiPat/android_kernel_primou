b0d6767c30e7581482c8419aef22b7370bd6c600
AdiPat <adithemagnificent@gmail.com>
2012-06-24 14:40:10 +0530

    Misc change

--- copy.sh
+++ copy.sh
@@ -1,13 +1,13 @@
 echo "Copying"
-cp arch/arm/common/cpaccess.ko ../Ramdisk/lib/modules/cpaccess.ko 
-cp arch/arm/mach-msm/reset_modem.ko ../Ramdisk/lib/modules/reset_modem.ko 
-cp arch/arm/mach-msm/dma_test.ko ../Ramdisk/lib/modules/dma_test.ko 
-cp crypto/ansi_cprng.ko ../Ramdisk/lib/modules/ansi_cprng.ko 
-cp drivers/net/wireless/bcmdhd/bcmdhd.ko ../Ramdisk/lib/modules/bcmdhd.ko 
-cp drivers/net/wireless/libra/librasdioif.ko ../Ramdisk/lib/modules/librasdioif.ko 
-cp drivers/net/kineto_gan.ko ../Ramdisk/lib/modules/kineto_gan.ko 
-cp drivers/staging/ti-st/fm_drv.ko ../Ramdisk/lib/modules/fm_drv.ko 
-cd ../Ramdisk/lib/modules
+cp arch/arm/common/cpaccess.ko ../boot.img-ramdisk/lib/modules/cpaccess.ko 
+cp arch/arm/mach-msm/reset_modem.ko ../boot.img-ramdisk/lib/modules/reset_modem.ko 
+cp arch/arm/mach-msm/dma_test.ko ../boot.img-ramdisk/lib/modules/dma_test.ko 
+cp crypto/ansi_cprng.ko ../boot.img-ramdisk/lib/modules/ansi_cprng.ko 
+cp drivers/net/wireless/bcmdhd/bcmdhd.ko ../boot.img-ramdisk/lib/modules/bcmdhd.ko 
+cp drivers/net/wireless/libra/librasdioif.ko ../boot.img-ramdisk/lib/modules/librasdioif.ko 
+cp drivers/net/kineto_gan.ko ../boot.img-ramdisk/lib/modules/kineto_gan.ko 
+cp drivers/staging/ti-st/fm_drv.ko ../boot.img-ramdisk/lib/modules/fm_drv.ko 
+cd ../boot.img-ramdisk/lib/modules
 
 for i in $(find . | grep .ko | grep './')
 do
