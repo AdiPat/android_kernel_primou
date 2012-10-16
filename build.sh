1eb1817b1cf59a90d1b648866261aad5111042c8
AdiPat <adithemagnificent@gmail.com>
2012-10-16 17:12:35 +0530

    board-primou: Make changes for PMEM Accordingly for CM

--- build.sh
+++ build.sh
@@ -3,40 +3,44 @@ cd ..
 rm *.gz 
 rm *.img
 rm *.zip 
-cd Sense
-rm *.gz 
-rm *.img
-rm *.zip 
-cd ../CM10
-rm *.gz
-rm *.img
-rm *.zip
-cd ../Kernel 
+cd Kernel 
 echo "Start Compiling"
+############################################################################################################################
+echo "COMPILE KERNEL FOR SENSE" 
 make clean mrproper
 make titanium_defconfig 
-make -j32
+make -j84
 echo "Copy Modules to Sense Ramdisk"
 find -name '*.ko' -exec cp -av {} ../boot.img-ramdisk/lib/modules/ \;
-echo ".."
-echo ".."
-echo ".."
-echo ".."
-echo ".."
-echo ".."
-echo ".."
-echo ".."
+echo "Pack Sense Ramdisk"
+tools/mkbootfs boot.img-ramdisk | gzip > boot.img-ramdisk-sense.gz
+echo "Pack Sense boot.img"
+./tools/mkbootimg --kernel Kernel/arch/arm/boot/zImage --ramdisk boot.img-ramdisk-sense.gz -o sense_boot.img --base 13f00000
+#############################################################################################################################
 sleep 5 
+#############################################################################################################################
+echo "Compile KERNEL for CM9/CM10"
+make clean mrproper
+make titanium_cm_defconfig
+make -j84
 echo "Copy Modules to CM10 Ramdisk"
 find -name '*.ko' -exec cp -av {} ../boot-img-ramdisk-cm10/lib/modules/ \;
 cd ..
-echo "Pack Sense Ramdisk"
-tools/mkbootfs boot.img-ramdisk | gzip > boot.img-ramdisk-sense.gz
 echo "Pack CM10 Ramdisk"
 tools/mkbootfs boot-img-ramdisk-cm10 | gzip > boot.img-ramdisk-cm10.gz
-echo "Pack Sense boot.img"
-./tools/mkbootimg --kernel Kernel/arch/arm/boot/zImage --ramdisk boot.img-ramdisk-sense.gz -o Sense/boot.img --base 13f00000
 echo "Pack CM10 boot.img" 
-./tools/mkbootimg --kernel Kernel/arch/arm/boot/zImage --ramdisk boot.img-ramdisk-cm10.gz -o CM10/boot.img --base 13f00000
+./tools/mkbootimg --kernel Kernel/arch/arm/boot/zImage --ramdisk boot.img-ramdisk-cm10.gz -o cm10_boot.img --base 13f00000
+
+######## CM10 DONE ####################
+
+echo "Copy Modules to CM9 Ramdisk"
+find -name '*.ko' -exec cp -av {} ../boot-img-ramdisk-cm9/lib/modules/ \;
+cd ..
+echo "Pack CM9 Ramdisk"
+tools/mkbootfs boot-img-ramdisk-cm10 | gzip > boot.img-ramdisk-cm9.gz
+echo "Pack CM9 boot.img" 
+./tools/mkbootimg --kernel Kernel/arch/arm/boot/zImage --ramdisk boot.img-ramdisk-cm9.gz -o cm9_boot.img --base 13f00000
+
+
 echo "Done"
 read ANS
